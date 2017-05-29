ac4ffc183bd91ff2b9a86a5b9a9b2992a58f4466
rent69 <rent.sidon@gmail.com>
2017-05-29 15:16:16 -0700

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,14 @@ var redux = require('redux');
 
 console.log('Starting redux-example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {     //ES6 syntax
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {     //ES6 syntax
 //  state = state || {name: 'Anonymous'};   ES5 syntax
 switch (action.type) {
   case 'CHANGE_NAME':
@@ -10,6 +17,39 @@ switch (action.type) {
       ...state,
       name: action.name
     };
+    case 'ADD_HOBBY':
+    return {
+      ...state,
+      hobbies: [
+        ...state.hobbies,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ]
+    };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+    hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
+    case 'ADD_MOVIE':
+    return {
+      ...state,
+      movies: [
+        ...state.movies,
+        {
+          id: nextMovieId++,
+          title: action.title,
+          genre: action.genre
+        }
+      ]
+    };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
    default:
      return state;
   }
@@ -25,6 +65,8 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -36,6 +78,37 @@ store.dispatch({
   name: 'Rent'
 });
 
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Terminator',
+  genre: 'Science Fiction'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Tootsie',
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
